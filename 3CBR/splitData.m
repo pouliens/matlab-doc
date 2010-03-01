f3759883e6444217d98d9c35261cc16cfec9205d
zell117 <zell117@30b1f3c4-0f2f-11df-a223-d798cd56f61e>
2010-03-01 15:23:38 +0000

    had the wrong split data file last time#

---
+++ 3CBR/splitData.m
@@ -0,0 +1,24 @@
+function [ trainingSet, validationSet, trainingTargets, validationTargets ] = splitData( dataSet, targets, nthDivision )
+% Set validationSet to rows of dataSet for nthDivision 10-fold, with rest forming trainingSet 
+
+[m, n] = size(dataSet);
+
+validationSet = [];
+trainingSet = [];
+trainingTargets = [];
+validationTargets = [];
+
+offset = m / 10;
+base = (nthDivision-1)*offset + 1;
+divisionSet = [base:1:base+(offset-1)];
+
+for i=1:m
+    if(ismember(i,divisionSet))
+        validationSet = vertcat(validationSet,dataSet(i, :));
+		validationTargets = vertcat(validationTargets,targets(i));
+    else
+        trainingSet = vertcat(trainingSet, dataSet(i, :));
+		trainingTargets = vertcat(trainingTargets,targets(i));
+    end
+
+end
