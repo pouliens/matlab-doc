4c2f7a6a08e718a836f90759bdb5a57415056e95
zell117 <zell117@30b1f3c4-0f2f-11df-a223-d798cd56f61e>
2010-03-10 23:52:43 +0000

    changes to similarity code with modified scoring algorithm - much better results

--- trunk/3CBR/similarity.m
+++ trunk/3CBR/similarity.m
@@ -1,20 +1,22 @@
-
 %uses jaccard simmilarity coefficient
 
-function [Sim] = similarity(newcase, oldcase)
+function [Sim] = similarity(newcase, oldcase, index)
+
+% {
 %denom = max(length(newcase.problem),length(oldcase.problem));
 intersecting = intersect(newcase.problem, oldcase.problem);
 numer = length(intersecting);
-
 denom = length(intersecting) + length(removeItems(newcase.problem, intersecting)) + length(removeItems(oldcase.problem, intersecting));
-
 Sim = numer / denom;
-end
+%}
 
-function [vector] = removeItems(fromVector, itemsToRemove)
-fromVector(ismember(fromVector, itemsToRemove)) = [];
-vector = fromVector;
-end
+% deprecated code here 
+%{
+% Sim = proportion of shared AUs relative to largest vector
+denom = max(length(newcase.problem),length(oldcase.problem));
+numer = length(intersect(newcase.problem,oldcase.problem));
+Sim = numer / denom;
+%}
 
 
 %{
@@ -26,3 +28,24 @@ missingScore = length(missing);
 Sim = matchingScore - missingScore;
 %}
 
+
+% Sim = typicality*number of AUs in common - (typicality*number of different AUs not in
+% cluster's index) + number of different AUs in clusters index - all
+% normalised by a max score
+maxScore = (oldcase.typicality+1)*max(length(newcase.problem),length(oldcase.problem));
+intersection = intersect(newcase.problem,oldcase.problem);
+matchingScore = length(intersection)*(oldcase.typicality+1);
+missing = setdiff(union(newcase.problem,oldcase.problem),intersection);
+missingScore = length(missing)*(oldcase.typicality+1);
+missButInIndex = intersect(missing,index);
+missingButInIndexScore = length(missButInIndex);
+Sim = (matchingScore - missingScore + missingButInIndexScore)/maxScore;
+
+
+end
+
+
+function [vector] = removeItems(fromVector, itemsToRemove)
+fromVector(ismember(fromVector, itemsToRemove)) = [];
+vector = fromVector;
+end
