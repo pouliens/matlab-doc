28ef94d5b6b50564f1459e1afdc2f3a00bbdb31d
philip.meier06 <philip.meier06@30b1f3c4-0f2f-11df-a223-d798cd56f61e>
2010-03-10 17:00:10 +0000

    Added another basic similarity (commented out) which prodcues slightly better results

--- trunk/3CBR/similarity.m
+++ trunk/3CBR/similarity.m
@@ -1,4 +1,16 @@
 function [Sim] = similarity(newcase, oldcase)
+
+% Sim = proportion of shared AUs relative to largest vector
 denom = max(length(newcase.problem),length(oldcase.problem));
 numer = length(intersect(newcase.problem,oldcase.problem));
 Sim = numer / denom;
+
+%{
+% Sim = 2*number of AUs in common - number of different AUs
+intersection = intersect(newcase.problem,oldcase.problem);
+matchingScore = length(intersection)*2;
+missing = setdiff(union(newcase.problem,oldcase.problem),intersection);
+missingScore = length(missing);
+Sim = matchingScore - missingScore;
+%}
+
